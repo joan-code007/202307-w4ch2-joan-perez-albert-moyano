7872e9d896dd3e545f9a7fb59d97b6275370bd2e
Albert Colubi <albertmc89@gmail.com>
2023-08-02 01:18:24 +0200

    Add styles to CharacterList

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,8 +5,22 @@ const characterData: Character[] = [
     name: "Luke Skywalker",
     height: "172",
     mass: "77",
-    created: "2014-12-09T13:50:51.644000Z",
+    created: "9 diciembre de 2014, a las 13:50:51",
     pictureUrl: "https://starwars-visualguide.com/assets/img/characters/1.jpg",
+  },
+  {
+    name: "Boba Fett",
+    height: "183",
+    mass: "78.2",
+    created: "15 de diciembre de 2014, a las 12:49:32",
+    pictureUrl: "https://starwars-visualguide.com/assets/img/characters/22.jpg",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    created: "10 de diciembre de 2014, a las 16:20:44",
+    pictureUrl: "https://starwars-visualguide.com/assets/img/characters/11.jpg",
   },
 ];
 
